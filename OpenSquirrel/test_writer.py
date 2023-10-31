ad77d6c2bf13fdc7097bd5abdacbbb639a7fb700
pablolh <p.lehenaff@tudelft.nl>
2023-10-31 15:46:34 +0100

    Add writer test

---
+++ OpenSquirrel/test_writer.py
@@ -0,0 +1,36 @@
+import unittest
+from TestGates import TEST_GATES
+from SquirrelAST import SquirrelAST
+from Writer import Writer
+
+class WriterTest(unittest.TestCase):
+    def test_write(self):
+        squirrelAST = SquirrelAST(TEST_GATES, 3, "myqubitsregister")
+
+        writer = Writer(TEST_GATES)
+
+        written = writer.process(squirrelAST)
+        
+        assert written == """version 3.0
+
+qubit[3] myqubitsregister
+
+"""
+
+        squirrelAST.addGate("h", 0)
+        squirrelAST.addGate("cr", 0, 1, 1.234)
+
+        written = writer.process(squirrelAST)
+
+        assert written == """version 3.0
+
+qubit[3] myqubitsregister
+
+h q[0]
+cr q[0], q[1], 1.234
+"""
+
+
+if __name__ == '__main__':
+    unittest.main()
+
