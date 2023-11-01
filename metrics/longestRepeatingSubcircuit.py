f79d6338a79bab748ef32c2445028cee60a78d39
pablolh <p.lehenaff@tudelft.nl>
2023-11-01 13:47:22 +0100

    Fix memory usage of longest repeating subcircuit
    Use a sublist view, avoids copying data over and over again

--- metrics/longestRepeatingSubcircuit.py
+++ metrics/longestRepeatingSubcircuit.py
@@ -7,6 +7,77 @@ def longestCommonPrefix(s1, s2):
             return i
     return min(len(s1), len(s2))
 
+class SublistViewIterator:
+    def __init__(self, sublistView):
+        self.sublistView = sublistView
+        self.current = 0
+
+    def __next__(self):
+        if self.current < len(self.sublistView):
+            resultIndex = self.current
+            self.current += 1
+            return self.sublistView.data[resultIndex]
+        
+        raise StopIteration
+
+
+class SublistView:
+    def __init__(self, data, start, end):
+        assert end is None or start <= end
+        assert end is None or end < len(data)
+
+        self.data = data
+        self.start = start
+        self.stop = end
+        self.currentIndex = start
+    
+    def __len__(self):
+        if self.stop is not None and self.start is not None:
+            return self.stop - self.start
+        elif self.start is not None:
+            return len(self.data) - self.start
+        elif self.stop is not None:
+            return self.stop
+        else:
+            assert False
+    
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            assert key.step is None, "Unimplemented: non-1 step"
+            assert key.stop is None or key.stop < len(self)
+            assert key.start is None or key.start < len(self)
+
+            if self.start is None and key.start is None:
+                subStart = None
+            elif self.start is None and key.start is not None:
+                subStart = key.start
+            elif self.start is not None and key.start is None:
+                subStart = self.start
+            else:
+                subStart = self.start + key.start
+            
+            assert subStart is None or subStart < len(self.data)
+
+            if key.stop is None:
+                subStop = self.stop
+            else:
+                assert key.stop < len(self)
+                subStop = (0 if self.start is None else self.start) + key.stop
+
+            assert subStop is None or subStop < len(self.data)
+
+            return SublistView(self.data, subStart, subStop)
+
+        assert key < len(self)
+        return self.data[self.start + key]   
+
+    def __iter__(self):
+        return SublistViewIterator(self)
+
+    def __list__(self):
+        return self.data[slice(self.start, self.stop, 1)]
+
+
 class SuffixTree:
     def __init__(self):
         self.instructions = []
@@ -16,27 +87,27 @@ class SuffixTree:
     def __repr__(self):
         return "{ " + repr(self.instructions) + ("(" + str(self.position) + ")" if self.position != -1 else "") + " - " + ", ".join(map(repr, self.children)) + "}"
 
-    def add(self, instr: list[CQASMParser.Instruction], position: int):
-        for c in self.children:
-            if c is not None:
-                cp = longestCommonPrefix(instr, c.instructions)
-                if cp == len(c.instructions):
-                    c.add(instr[len(c.instructions):], position)
+    def add(self, instr: SublistView, position: int):
+        for child in self.children:
+            if child is not None:
+                cp = longestCommonPrefix(instr, child.instructions)
+                if cp == len(child.instructions):
+                    child.add(instr[len(child.instructions):], position)
                     return
                 
                 if cp > 0:
                     newChild1 = SuffixTree()
-                    newChild1.instructions = c.instructions[cp:]
-                    newChild1.children = c.children
-                    newChild1.position = c.position
+                    newChild1.instructions = child.instructions[cp:]
+                    newChild1.children = child.children
+                    newChild1.position = child.position
 
                     newChild2 = SuffixTree()
                     newChild2.instructions = instr[cp:]
                     newChild2.position = position
                     
-                    c.instructions = c.instructions[:cp]
-                    c.position = -1
-                    c.children = [newChild1, newChild2]
+                    child.instructions = child.instructions[:cp]
+                    child.position = -1
+                    child.children = [newChild1, newChild2]
                     return
 
 
@@ -57,11 +128,12 @@ class SuffixTree:
         subresults = map(lambda c: c.deepestInternalNode(), filter(lambda c: c is not None, self.children))
 
         m = max(subresults, key = lambda x: len(x[0]))
-
-        if m[0] == []:
-            return (self.instructions, len(self.children))
         
-        return (self.instructions + m[0], m[1])
+        if len(m[0]) == 0:
+            return (list(self.instructions), len(self.children))
+        
+        return (list(self.instructions) + m[0], m[1])
+
 
 
 # naive construction of suffix tree
@@ -69,7 +141,7 @@ def longestRepeatingSubcircuit(c: list[CQASMParser.Instruction]):
     t = SuffixTree()
     for i in range(len(c)):
         start = len(c) - i - 1
-        t.add(instr=c[start:], position=start)
+        t.add(instr=SublistView(c, start, None), position=start)
 
     deepestInternalNode = t.deepestInternalNode()
     return { "LongestRepeatingSubcircuit": deepestInternalNode[0], "NumberOfRepetitionsOfLongestRepeatingSubcircuit": deepestInternalNode[1] if deepestInternalNode[0] != [] else 0 }
--- processFiles.py
+++ processFiles.py
@@ -4,23 +4,23 @@ from parsing import CQASMParser
 from metrics.longestRepeatingSubcircuit import longestRepeatingSubcircuit
 from metrics.paths import getPathStats
 from multiprocessing import Pool, Value
-from threading import Lock
 
 OUTPUT_FILE = os.path.dirname(os.path.realpath(__file__)) + "/result.csv"
 
 
-# MAX_NUM_LINES = 99999
+# MAX_NUM_LINES = 50000
 # files = []
 
 # for f in glob.glob(os.path.dirname(os.path.realpath(__file__)) + "/metrics/data/*.qasm"):
 #     num_lines = sum(1 for _ in open(f))
 
-#     if num_lines <= MAX_NUM_LINES:
+#     if num_lines > MAX_NUM_LINES:
 #         files.append(f)
 
 files = glob.glob(os.path.dirname(os.path.realpath(__file__)) + "/metrics/data/*.qasm")
 
-lock = Lock()
+# files = ["/shares/bulk/plehenaff/cQASM-tools/metrics/data/q=11_s=89_2qbf=022_1.qasm"]
+
 counter = Value('i', 0)
 
 statistics = sorted([
@@ -37,7 +37,6 @@ statistics = sorted([
 ])
 
 def writeToFile(data: dict):
-    lock.acquire()
     with open(OUTPUT_FILE, "a") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=statistics)
         assert(sorted(data.keys()) == statistics)
@@ -46,8 +45,6 @@ def writeToFile(data: dict):
     global counter
     counter.value += 1
     print(f"Done {counter.value} / {len(files)} files")
-    lock.release()
-
 
 def processFile(fileName):
     try:
@@ -77,17 +74,20 @@ def processFile(fileName):
         
     except Exception as e:
         print(f"File {fileName} gave error: {e} and was not processed")
-        lock.release()
+
         return
+    
+
 
 if __name__ == "__main__":
-    print(f"Will process files: {files}")
+    # print(f"Will process files: {files}")
 
     with open(OUTPUT_FILE, "w") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=statistics)
         writer.writeheader()
     
     # for f in files:
+    #     print(f"processing {f}")
     #     processFile(f)
     
     with Pool(8, initargs = (counter, )) as p:
